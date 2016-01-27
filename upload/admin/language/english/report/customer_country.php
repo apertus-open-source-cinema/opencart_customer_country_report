24e3ee3be1829e6afebd2ccce8d0e2bf44dc8119
Sebastian Pichelhofer <sebastian.pichelhofer@gmail.com>
2016-01-27 15:06:52 +0100

    initial commit

---
+++ upload/admin/language/english/report/customer_country.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']         = 'Customer Country Report';
+
+// Text
+$_['text_list']             = 'Customer Country List';
+$_['text_all_status']       = 'All Statuses';
+
+// Column
+$_['column_country']        = 'Country';
+$_['column_orders']         = 'No. Orders';
+$_['column_total']          = 'Total';
+
+// Entry
+$_['entry_date_start']      = 'Date Start';
+$_['entry_date_end']        = 'Date End';
+$_['entry_status']          = 'Order Status';
